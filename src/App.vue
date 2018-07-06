602d0600f646d21501d0dfb5facc904e6d267ed8
Addison <perhaps0660@gmail.com>
2018-07-06 17:54:51 +1000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="container">
+        <SearchBar @termChange="onTermChange"></SearchBar>
+        <div class="row">
+            <VideoDetail :video="selectedVideo"></VideoDetail>
+            <VideoList :videos='videos' @videoSelect="onVideoSelect"></VideoList>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetail';
+
+const API_KEY = 'AIzaSyAdXnTuvcFcqpBVg5UXTuzNcALUwgdGDYM';
+
+export default {
+    name: 'App',
+    components: {
+        SearchBar,
+        VideoList,
+        VideoDetail,
+    },
+    data() {
+        return {
+            videos: [],
+            selectedVideo: null,
+        }
+    },
+    methods: {
+        onTermChange(searchTerm) {
+            axios.get('https://www.googleapis.com/youtube/v3/search', {
+                params: {
+                    key: API_KEY,
+                    type: 'video',
+                    part: 'snippet',
+                    q: searchTerm
+                }
+            }).then(response => {
+                this.videos = response.data.items
+            });
+        },
+        onVideoSelect(video) {
+            this.selectedVideo = video;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/VideoList.vue
@@ -0,0 +1,38 @@
+<template>
+    <ul class="list-group col-md-4">
+        <VideoListItem 
+        v-for="(video,index) in videos" 
+        :video="video" 
+        :key="index" 
+        @videoSelect="onVideoSelect">
+        </VideoListItem>
+    </ul>
+</template>
+
+<script>
+import VideoListItem from './VideoListItem';
+
+export default {
+    name: 'VideoList',
+    components: {
+        VideoListItem
+    },
+    props: {
+        videos: Array //Prop type validation check
+    },
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+        onVideoSelect(video) {
+            this.$emit("videoSelect", video)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
